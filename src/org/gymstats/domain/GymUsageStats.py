17ed1f3c23817e0af94a244cb5e7b416c8ec9104
Mika Niinistö <mika.niinisto@gmail.com>
2022-07-11 11:38:24 +0300

    Merge weather data into gym stats data
    - Modify the data of both stats so, that we can match them
    - Do inner merge of the data, having only data on common hours

--- src/org/gymstats/domain/GymUsageStats.py
+++ src/org/gymstats/domain/GymUsageStats.py
@@ -7,13 +7,23 @@ class GymUsageStats:
     Container for the data derived from the GymStatisticsFile
     """
     def __init__(self, dataframe: pandas.DataFrame) -> None:
-        self._dataframe = dataframe.copy()
-        self._dataframe['time'] = pandas.to_datetime(self._dataframe['time'])
+        self._dataframe = dataframe.copy(deep=True)
+        self._dataframe['time'] = pandas.to_datetime(self._dataframe['time'], utc=True)
         self._dataframe = self._dataframe.set_index('time')
         self._device_colums = [column for column in self._dataframe.columns if column != 'time']
-        self._dataframe['total'] = self.summarize_columns(self._device_colums)
-        self._dataframe['hour'] = self._dataframe.index.hour
-        self._dataframe['weekday'] = self._dataframe.index.weekday
+        self._dataframe['Total'] = self.summarize_columns(self._device_colums)
+        self._dataframe['Year'] = self._dataframe.index.year
+        self._dataframe['Month'] = self._dataframe.index.month
+        self._dataframe['Day'] = self._dataframe.index.day
+        self._dataframe['Hour'] = self._dataframe.index.time
+        self._dataframe['Timezone'] = 'UTC'
+        self._dataframe['Weekday'] = self._dataframe.index.weekday
+
+    def print_hour(self, index):
+        hours = []
+        for i in range(index.hour.size):
+            hours.append('{0}:{1}'.format(index.hour.loc[i], index.minute.loc[i]))
+        return hours
 
     def summarize_columns(self, columns: list) -> int:
         """Calculate sum of given column values"""
--- src/org/gymstats/services/WeatherImpact.py
+++ src/org/gymstats/services/WeatherImpact.py
@@ -1,5 +1,7 @@
 import pandas
 
+from org.gymstats.domain.GymUsageStats import GymUsageStats
+
 
 class WeatherImpact:
     """Analyzes the weather impact on gym popularity
@@ -16,10 +18,33 @@ class WeatherImpact:
             weather: pandas.DataFrame - Weather stats in Kaisaniemi
         """
         self._devices = devices
-        self._stats = stats.copy()
         self._weather = weather.copy()
+        self._stats = stats.copy()
+        self.format_weather()
+        self.group_stats_hourly()
+        # merge our data, inner method... so only common rows are present
+        self._merged = pandas.merge(self._stats, self._weather, on=['Year', 'Month', 'Day', 'Hour', 'Timezone'])
+
+    def format_weather(self):
+        """Format weather hours to match stats hours"""
+        self._weather['time'] = pandas.to_datetime(self._weather['Hour'], utc=True)
+        self._weather = self._weather.set_index('time')
+        self._weather['Hour'] = self._weather.index.time
+        self._weather = self._weather.reset_index()
+        self._weather = self._weather.drop(['time'], axis='columns')
+
+    def group_stats_hourly(self):
+        """Group by hourly use and enrich the gym stats"""
+        self._stats['time'] = pandas.to_datetime(self._stats['time'], utc=True)
+        self._stats = self._stats.groupby(pandas.Grouper(key='time', freq='1H')).sum()
+        self._stats = self._stats.reset_index()
+        # Enrich the data... *after* we've grouped data hourly
+        self._usage = GymUsageStats(self._stats)
+        self._stats = self._usage.dataframe.copy()
+        self._stats = self._stats.reset_index()
 
     def temperature_impact(self):
+        print(self._merged)
         pass
 
     def precipitation_impact(self):
